d9e16bf7682a33131776071fe700bb162014233d
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-14 00:34:18 -0500

    updated db queries

--- administration/insert_categories.php
+++ administration/insert_categories.php
@@ -14,6 +14,32 @@ if(isset($_POST['insert_device'])){
     $product_image=$_FILES['device_image']['name'];
 
     //accessing image temp name
-    $tmp_image=$_FILES['device_image']['tmp_name']
+    $tmp_image=$_FILES['device_image']['tmp_name'];
+
+    //checking if it is empty
+    if($device_title=='' or $description=='' or $device_keywords=='' or $device_category=='' or $device_price=='' or $device_image==''){
+
+        echo"<script>alert('Please fill all the available fields!')</script>";
+        exit();
+
+    }else{
+            move_uploaded_file($tmp_image,"./product_images/$device_image");
+
+            //insert products
+            $insert_devices="insert into `products` (product_title,product_description,product_keywords,category_id,product_image,product_price,date,status) values ('$product_title','$description','$product_keywords','$product_category','$product_image','$product_price',NOW(),'$product_status')";
+
+            $result_query=mysqli_query($con,$insert_devices);
+            if($result_query){
+
+                echo "<script>alert('Successfully inserted the products')</script>";
+
+        }
+
+    }
+
+    
+
+
+}
 }
 ?>
