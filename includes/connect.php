71e6fb9c05e5c48682678580aea3c43ed7223528
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-14 13:41:12 -0500

    updated registration

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect('localhost','root','','e-commerce');
+$con=mysqli_connect('localhost','root','123','e-commerce');
 if(!$con){
    die("Connection failed: " . mysqli_connect_error());
    //die(mysqli_error($con));
