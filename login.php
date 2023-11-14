da0904689f7093e22f87493d6e74616bbfab527f
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-13 20:14:48 -0500

    database query has been created for the index.php

--- login.php
+++ login.php
@@ -1,6 +1,30 @@
 <?php
 include('includes/connect.php');
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    $selectUserQuery = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($con, $selectUserQuery);
+
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $hashedPassword = $row['password'];
+
+        if (password_verify($password, $hashedPassword)) {
+            session_start(); 
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['username'] = $row['username'];
+
+            echo "<script>alert('Login successful. You are now logged in.')</script>";
+            echo "<script>window.location.href='index.php';</script>";
+        } else {
+            echo "<script>alert('Invalid email or password. Please try again.')</script>";
+        }
+    } else {
+        echo "<script>alert('User not found. Please register or check your email.')</script>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
