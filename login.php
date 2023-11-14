8c4f9b2183ca06e80b34efd5f8d6b54b83e132ff
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-13 20:17:11 -0500

    password input has been created

--- login.php
+++ login.php
@@ -102,14 +102,17 @@ if (isset($_POST['login'])) {
 <body>
     <div class="container">
         <h3 class="text-center" style="color: #007bff; padding: 0px; font-size: 28px; font-weight: bold; font-family: 'Arial', sans-serif; text-transform: uppercase; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">PY-Electronics</h3>
-       
         <h2 class="text-center" style="color: black;">Log In</h2>
         <form action="" method="post" class="mb-2">
             <div class="input-group">
                 <label for="email" class="input-group-text" id="basic-addon2"><i class="fa-solid fa-envelope"></i> Email:</label>
                 <input type="email" class="form-control" name="email" id="email" placeholder="parminder@gmail.com" aria-label="Email" aria-describedby="basic-addon2">
             </div>
-            
+            <div class="input-group">
+                <label for="password" class="input-group-text" id="basic-addon3"><i class="fa-solid fa-lock"></i> Password:</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="" aria-label="Password" aria-describedby="basic-addon3">
+            </div>
+            <button class="my-button" style="font-size: 20px;" name="login">Login</button>
         </form>
         <p style="font-size: 14px;">Don't have an account? <a class="register-link" href="registration.php">Create Account</a></p>
     </div>
