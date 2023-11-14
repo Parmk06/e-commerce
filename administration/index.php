96fca1792c0c3fc84bc6ff8149d60a71ea3adbe1
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-14 00:02:27 -0500

    third row created for index admin with all buttons

--- administration/index.php
+++ administration/index.php
@@ -34,5 +34,56 @@
         color: white;
     }
     </style>
-
 </head>
+<body>
+    <!-- navbar -->
+<div class="container-fluid p-0">
+    <!-- First child -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <div class="container-fluid">
+
+            <img src="../images/admin-logo" alt="" class="logo">
+
+            <nav class="navbar navbar-expand-lg">
+                
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="" class="nav-link" style="color: black;">
+                            <B><h5>Welcome!, <?php echo $_SESSION['admin_username']; ?></h5></B>
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+    </nav>
+</div>
+<div class="bg-light">
+        <h3 class="text-center p-2" style="color: black; padding: 5px; font-size: 36px; font-weight: bold; font-family: 'Arial', sans-serif; text-transform: uppercase; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">Manage Details</h3>
+    </div>
+
+    <!-- Third Child -->
+    <div class="row">
+        <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+            <div class="p-5"> 
+                <a href="#"><img src="../images/admin-logo.jpg" alt="" class="admin_image"></a>
+                <p class="text-light text-center">Admin SAP</p>
+            </div>
+
+            <div class="button text-center">
+            
+                <button class="my-button" ><a href="insert_product.php" class="nav-link">Insert Products</a></button>
+
+                <button class="my-button"><a href="index.php?insert_category" class="nav-link">Insert Categories</a></button>
+
+                <button class="my-button"><a href="view_categories.php" class="nav-link">View Categories</a></button>
+
+                <button class="my-button"><a href="" class="nav-link">All Orders</a></button>
+
+                <button class="my-button"><a href="logout_admin.php" class="nav-link">Logout</a></button> 
+            </div>
+        </div>
+    </div>
+
+</body>
