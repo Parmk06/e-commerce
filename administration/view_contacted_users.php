9d795b4ea459da77fe0b87a18beb629c778836b2
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-20 08:17:06 -0500

    css has been added to viewcontactedusers

--- administration/view_contacted_users.php
+++ administration/view_contacted_users.php
@@ -11,7 +11,57 @@ include('../includes/connect.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="../style.css">
-    
+<style>
+body {
+    background-color: #f4f4f4;
+    font-family: Arial, sans-serif;
+}
+
+.container {
+    max-width: auto;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+h1 {
+    color: #ff0000;
+    text-align: center;
+}
+
+table {
+    width: 200%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    background-color: #fff;
+}
+
+table, th, td {
+    border: 1px solid #ccc;
+}
+
+th, td {
+    padding: 10px;
+    text-align: left;
+}
+
+td {
+    background-color: #007bff;
+    color: #fff;
+    }
+
+tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+p {
+    text-align: center;
+    font-weight: bold;
+}
+
+.container {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
 </style>
 </head>
 <body class="bg-light">
