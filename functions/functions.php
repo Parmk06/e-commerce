138e535c783b6dd0ca33af06bdf987833ab9b82e
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-20 09:08:59 -0500

    added getItAddress fucntion

--- functions/functions.php
+++ functions/functions.php
@@ -155,7 +155,21 @@ function getcategories(){
 }
 //get ip address function
 
-
+function getIPAddress() {  
+  //whether ip is from the share internet  
+   if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+              $ip = $_SERVER['HTTP_CLIENT_IP'];  
+      }  
+  //whether ip is from the proxy  
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+              $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+   }  
+  //whether ip is from the remote address  
+  else{  
+           $ip = $_SERVER['REMOTE_ADDR'];  
+   }  
+   return $ip;  
+  }  
 function cart(){
   if(isset($_GET['add_to_cart'])){
     global $con;
